63bf0ec19bbe79ec9d8cfd937fd814d54c7c4f91
Soundripper <jerrsoundripper@gmail.com>
2022-05-31 16:19:50 +0200

    AdvFilters Add remove OK

--- advFilters.js
+++ advFilters.js
@@ -1,4 +1,4 @@
-// let totalFilters = [];
+let totalFilters = [];
 
 ///////////////////////////////////////////////////////////////////////////////
 // Gestionnaire Input advanced FILTER//
@@ -29,11 +29,11 @@ dropMenus.forEach(dropMenu =>{
 
 ///////////////////////////////////////////////////////////////////////////////
 // Gestionnaire Ajout Tags //
-const tagClickListen = () => {
+const tagClickInit = () => {
+    console.log("tagClickInit");
     filterBadgesWrapper = document.getElementById('filterBadgesWrapper');
     btnTags = document.querySelectorAll('.dropdown-item');
     btnTags.forEach(elt => {
-        // console.log(elt);
         elt.addEventListener('click', (e) => {
             const itemName = e.target.innerHTML;
             const newBtn = document.createElement('span');
@@ -61,68 +61,96 @@ const tagClickListen = () => {
                 </button>
             `
             }
+            console.log(newBtn);
+            tagCloseAdd(newBtn);
             e.target.remove();
             filterBadgesWrapper.appendChild(newBtn);
-            console.log(newBtn);
-            tagUpdate();
-            
         })
     })
 }
 
-const tagUpdate = () => {
+const tagClickAdd = (newBtn) => {
     // tagsFiltering();
-    activeTagsClose = document.querySelectorAll('.bi-x-circle');
-    // console.log(activeTagsClose);
-    activeTagsClose.forEach(elt => {
-        elt.addEventListener('click', (e) => {
-            tagspan = e.target.parentNode.querySelector('span');
-            // tagName = tagspan.innerHTML;
+    console.log("tagClickAdd");
+    newBtn.addEventListener('click', (e) => {
+            const itemName = e.target.innerHTML;
+            const newBtn = document.createElement('span');
+            if (e.target.classList.contains('ing')) {
+                newBtn.innerHTML = `
+                <button type="button" class="btn btn-primary position-relative me-2 mb-2 tag">
+                <span> ${itemName} </span> &nbsp
+                <i class="bi bi-x-circle"></i>
+                </button>
+            `
+            }
+            else if (e.target.classList.contains('app')) {
+                newBtn.innerHTML = `
+                <button type="button" class="btn btn-success position-relative me-2 mb-2 tag">
+                <span> ${itemName} </span> &nbsp
+                <i class="bi bi-x-circle"></i>
+                </button>
+            `
+            }
+            else if (e.target.classList.contains('ust')) {
+                newBtn.innerHTML = `
+                <button type="button" class="btn btn-danger position-relative me-2 mb-2 tag">
+                <span> ${itemName} </span> &nbsp
+                <i class="bi bi-x-circle"></i>
+                </button>
+            `
+            }
+            tagCloseAdd(newBtn);
+            e.target.remove();
+            filterBadgesWrapper.appendChild(newBtn);
+            // tagsFiltering();
+        })
+    }
+
+const tagCloseAdd = (btn) => {
+    // tagsFiltering();
+    console.log("tagCloseAdd");
+    btn.addEventListener('click', (e) => {
             let itemName = e.target.parentNode.querySelector('span').innerHTML;
             itemName = itemName.trim();
-            // totalFilters = totalFilters.filter(item => item !== itemName);
-            // console.log(totalFilters);
             if (e.target.parentNode.classList.contains('btn-primary')) {
-                console.log(e.target.parentNode);
                 wrapper = document.querySelector('.ingChoices');
-                // console.log("Ingredient");
                 const btn = document.createElement('button');
                 btn.classList.add('dropdown-item', 'btn-primary', 'bg-primary', 'text-light', 'ing');
                 btn.innerHTML = itemName;
-                console.log(itemName);
                 wrapper.appendChild(btn);
+                tagClickAdd(btn);
             }
             else if (e.target.parentNode.classList.contains('btn-success')) {
                 wrapper = document.querySelector('.appChoices')
-                // console.log("Appareil");
                 const btn = document.createElement('button');
                 btn.classList.add('dropdown-item', 'btn-success', 'bg-success', 'text-light', 'app');
                 btn.innerHTML = itemName;
                 console.log(itemName);
                 wrapper.appendChild(btn);
+                tagClickAdd(btn);
             }
             else if (e.target.parentNode.classList.contains('btn-danger')) {
                 wrapper = document.querySelector('.ustChoices');
-                // console.log("Ustensile");
                 const btn = document.createElement('button');
                 btn.classList.add('dropdown-item', 'btn-danger', 'bg-danger', 'text-light', 'ust');
                 btn.innerHTML = itemName;
                 console.log(itemName);
                 wrapper.appendChild(btn);
+                tagClickAdd(btn);
             }
             e.target.parentNode.parentNode.remove();
             // tagsFiltering();
-            tagClickListen();
+            
         })
-    
-    })
+        
 }
 
-tagClickListen();
+tagClickInit();
 
 ///////////////////////////////////////////////////////////////////////////////
 // Gestionnaire Tags FILTER//
 const tagsFiltering = () => {
+    console.log("tagsFiltering");
     let activeTags = document.querySelectorAll('.tag');
     if (activeTags.length > 0){
         // console.log("sup à 0");
@@ -168,46 +196,3 @@ const tagsFiltering = () => {
         })
     }
 }
-
-///////////////////////////////////////////////////////////////////////////////
-// Gestionnaire Input main FILTER//
-// const inputGeneral = document.querySelector('.search_General');
-// Boucle For //
-//     const updateValue = (event) => {
-//     let startDate = Date.now();
-//     resultInput = event.target.value;
-//     let elements = document.getElementsByClassName('card');
-//     for(let i = 0; i<elements.length; i++){
-//         if(elements[i].innerHTML.toLowerCase().includes(resultInput.toLowerCase())){
-//             elements[i].style.display = 'flex';
-//         }
-//         else{
-//             elements[i].style.display = 'none';
-//         }
-//     }
-//     let endDate = Date.now();
-//     console.log(endDate - startDate);
-// }
-
-// Boucle forEach //
-// const generalSearchValue = (event) => {
-//     let startDate = Date.now();
-//     resultInput = event.target.value;
-//     console.log(resultInput);
-//     let elementsCard = document.getElementsByClassName('card');
-//     let arrCards = Array.from(elementsCard);
-//     // [...elements.forEach(element => {
-//     arrCards.forEach(elementsCard => {
-//             if(elementsCard.innerHTML.toLowerCase().includes(resultInput.toLowerCase())){
-//                 elementsCard.style.display = 'flex';
-//             }
-//             else{
-//                 elementsCard.style.display = 'none';
-//             }
-//     });
-//     let endDate = Date.now();
-//     console.log(endDate - startDate);
-// }
-// inputGeneral.addEventListener('input', generalSearchValue);
-
-
